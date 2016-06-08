96d51e9313a1a81b8ced9d5bea47a15449ac153c
krish <krish.sumaroo@gmail.com>
2016-06-08 17:32:41 -0400

    settings page

---
+++ app/pages/profilepage/profilepage.ts
@@ -0,0 +1,14 @@
+import {Page, NavController} from 'ionic-angular';
+
+/*
+  Generated class for the ProfilepagePage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/profilepage/profilepage.html',
+})
+export class ProfilepagePage {
+  constructor(public nav: NavController) {}
+}
